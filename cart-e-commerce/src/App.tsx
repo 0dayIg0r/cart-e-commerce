45b039a7a6a4714e3cc78d031b0dc87ef41213a6
0dayig0r <igormedeiros.f@gmail.com>
2023-07-04 20:46:10 -0300

    criando pastas e iniciando router dom

---
+++ cart-e-commerce/src/App.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import Cart from "./pages/cart";
+import Home from "./pages/home";
+import Layout from "./components/layout";
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
+
+export { router };
