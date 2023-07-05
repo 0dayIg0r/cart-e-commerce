ffa6348ed7cc018e280a21c799343886ce508f99
0dayig0r <igormedeiros.f@gmail.com>
2023-07-04 22:23:59 -0300

    iniciando apicontext

--- cart-e-commerce/src/main.tsx
+++ cart-e-commerce/src/main.tsx
@@ -4,9 +4,12 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./App";
+import CartProvider from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
   </React.StrictMode>
 );
