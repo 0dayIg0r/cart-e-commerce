e5d5f8c68da08170fff36260d1466c251a1703c4
0dayig0r <igormedeiros.f@gmail.com>
2023-07-04 21:58:36 -0300

    consumindo fake api json

--- cart-e-commerce/src/pages/home/index.tsx
+++ cart-e-commerce/src/pages/home/index.tsx
@@ -1,6 +1,27 @@
-
+import { useEffect, useState } from "react";
 import { BsCartPlus } from "react-icons/bs";
+
+import { api } from "../../services/api";
+
+interface ProductProps {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  cover: string;
+}
+
 function Home() {
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const res = await api.get("/products");
+      setProducts(res.data);
+    }
+
+    getProducts();
+  }, []);
   return (
     <div>
       <main className="w-full max-w-7xl px-4 mx-auto">
@@ -9,20 +30,25 @@ function Home() {
         </h1>
 
         <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-          <section className="w-full">
-            <img
-              className="w-full rounded-lg max-h-70 mb-2"
-              src="https://www.meliuz.com.br/blog/wp-content/uploads/2020/09/solo3-pdp-p9-club_navy.png.medium.1x-756x1024.png"
-              alt="Logo Produto"
-            />
-            <p className="font-medium mt-1 mb-2">Fone Beats</p>
-            <div className="flex gap-3 items-center">
-              <strong className="text-zinc-700/90"> R$ 1000</strong>
-              <button className="bg-zinc-900 p-1 rounded">
-                <BsCartPlus color="#fff" />
-              </button>
-            </div>
-          </section>
+          {products.map((product) => (
+            <section className="w-full" key={product.id}>
+              <img
+                className="w-full rounded-lg max-h-70 mb-2"
+                src={product.cover}
+                alt={product.title}
+              />
+              <p className="font-medium mt-1 mb-2">{product.title}</p>
+              <div className="flex gap-3 items-center">
+                <strong className="text-zinc-700/90">{product.price.toLocaleString('pt-BR',{
+                  style: 'currency',
+                  currency: 'BRL'
+                })}</strong>
+                <button className="bg-zinc-900 p-1 rounded">
+                  <BsCartPlus color="#fff" />
+                </button>
+              </div>
+            </section>
+          ))}
         </div>
       </main>
     </div>
